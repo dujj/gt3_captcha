8a62fefcbb0848db26dc61e81dad026e4cbb3ee0
dujj <dujj20507385@gmail.com>
2021-08-09 10:51:40 +0800

    add

--- test/gt3_captcha_test.dart
+++ test/gt3_captcha_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gt3_captcha/gt3_captcha.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('gt3_captcha');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await Gt3Captcha.platformVersion, '42');
   });
 }
